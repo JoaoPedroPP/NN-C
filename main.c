7cb3b3224db042cecd46fd62347454324e7f92ac
JoaoPedroPP <jp052@hotmail.com>
2021-03-14 17:04:28 -0300

    clean code

--- main.c
+++ main.c
@@ -7,12 +7,10 @@
 int main(int argc, char **argv)
 {
     Network *NN = CreateNetwork();
-    // addLayer(NN, 2, 2);
     NN->add(NN, 4,3);
     NN->add(NN, 3,2);
     NN->add(NN, 2,1);
     displayNetwork(NN->L);
-    // add(NN, 2,1);
 
     return 0;
 }
